593f22776f59f726d76dd002cd96e68fba9c35c3
Mathis <mathislef27@gmail.com>
2024-09-15 20:09:28 +0200

    feat(hello): add endpoint which say hello

--- src/main/java/com/lefevre/mathis/first/api/controller/FirstController.java
+++ src/main/java/com/lefevre/mathis/first/api/controller/FirstController.java
@@ -1,6 +1,8 @@
 package com.lefevre.mathis.first.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class FirstController {
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String ping(@PathVariable String name,@RequestParam(required = false) Integer age) {
+        if(age==null){return "Bien le bonjour " + name + "tu n'as pas d'age";}
+        return "Bien le bonjour " + name + " tu as " + age +"ans";
     }
 }
